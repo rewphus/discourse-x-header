61bd7ae68ef6d9cc120be789594a2f6849298ab3
rewphus <rcdurant@gmail.com>
2015-08-28 14:30:04 -0500

    first commit

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: Set X-Frame-Options correctly
+# about: Set site headers
+# version: 1
+# authors: Tom
+
+Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => 'http://polygonaltalk.github.io'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
